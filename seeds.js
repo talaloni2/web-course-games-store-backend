e10d454a2323eec156bc57eda6d3d38afd4bfc00
Tal Aloni <tal.aloni@planckresolution.com>
2022-12-23 09:55:59 +0200

    Modified to use async await

--- seeds.js
+++ seeds.js
@@ -9,21 +9,21 @@ const url = dbConfig.url;
 
 const mongoClient = new MongoClient(url);
 
-mongoClient.connect().then((_) => {
-    return new Promise((resolve, reject) => {const database = mongoClient.db(dbConfig.database);
+mongoClient.connect().then(async () => {
+
+    const database = mongoClient.db(dbConfig.database);
     const bucket = new GridFSBucket(database, {
-      bucketName: dbConfig.imgBucket,
+        bucketName: dbConfig.imgBucket,
     });
 
     const scrapedDataDir = "/Users/talaloni/PycharmProjects/colman/web-course-games-api-scraper/scraped_data/images";
     let fileNames = fs.readdirSync(scrapedDataDir);
-    
-    Promise.allSettled(fileNames.map((fileName) => {
+
+    await Promise.allSettled(fileNames.map(async (fileName) => {
         const fullPath = path.join(scrapedDataDir, fileName);
-        if (!fileName.endsWith(".jpg")){
+        if (!fileName.endsWith(".jpg")) {
             return;
         }
-        return new Promise( (resolve, reject)=> fs.createReadStream(fullPath).pipe(bucket.openUploadStream(fileName)).on("finish", resolve));
-    })).then(resolve);
-})
-}).then(process.exit);
+        return new Promise((resolve, reject) => fs.createReadStream(fullPath).pipe(bucket.openUploadStream(fileName)).on("finish", resolve));
+    }));
+}).then(() => mongoClient.close());
