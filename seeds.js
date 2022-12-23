9684a365b43c45474e29d36b47a6a96b6cf0c591
Tal Aloni <tal.aloni@planckresolution.com>
2022-12-23 09:42:05 +0200

    seeds now finishes after all files uploaded successfully.

--- seeds.js
+++ seeds.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 const path = require('path')
 const dbConfig = require("./config/db");
 
@@ -10,7 +10,7 @@ const url = dbConfig.url;
 const mongoClient = new MongoClient(url);
 
 mongoClient.connect().then((_) => {
-    const database = mongoClient.db(dbConfig.database);
+    return new Promise((resolve, reject) => {const database = mongoClient.db(dbConfig.database);
     const bucket = new GridFSBucket(database, {
       bucketName: dbConfig.imgBucket,
     });
@@ -18,11 +18,12 @@ mongoClient.connect().then((_) => {
     const scrapedDataDir = "/Users/talaloni/PycharmProjects/colman/web-course-games-api-scraper/scraped_data/images";
     let fileNames = fs.readdirSync(scrapedDataDir);
     
-    fileNames.forEach((fileName) => {
+    Promise.allSettled(fileNames.map((fileName) => {
         const fullPath = path.join(scrapedDataDir, fileName);
         if (!fileName.endsWith(".jpg")){
             return;
         }
-        fs.createReadStream(fullPath).pipe(bucket.openUploadStream(fileName));
-    })
-});
+        return new Promise( (resolve, reject)=> fs.createReadStream(fullPath).pipe(bucket.openUploadStream(fileName)).on("finish", resolve));
+    })).then(resolve);
+})
+}).then(process.exit);
