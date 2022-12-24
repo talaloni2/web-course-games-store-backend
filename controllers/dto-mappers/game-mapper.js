d982d53551169f137b3b5343f06e087f998ce1fe
Tal Aloni <tal.aloni@planckresolution.com>
2022-12-24 15:43:12 +0200

    attaching cover url to image response

--- controllers/dto-mappers/game-mapper.js
+++ controllers/dto-mappers/game-mapper.js
@@ -1,9 +1,18 @@
+const serverConfig = require("../../config/server");
+
+imagesUrl = serverConfig.filesURL;
+
+const getImageUrlIfExists = (imageId) => {
+    if (imageId === null || imageId === undefined) return null;
+    return `${imagesUrl}${imageId}.jpg`;
+}
+
 const mapToGamesList = (games) => {
     return games.map(game => {
         return {
             rating: game.totalRating || null,
             name: game.name,
-            cover: game.cover || null,
+            cover: getImageUrlIfExists(game.cover),
             id: game._id,
             price: game.price,
             availability: game.availability,
--- controllers/game.js
+++ controllers/game.js
@@ -4,9 +4,7 @@ const gamesMapper = require("./dto-mappers/game-mapper");
 const { buildGameListQuery } = require("./query-builders/game-query-builder");
 
 const gamesList = async (req, resp) => {
-
     const search = buildGameListQuery(req.query)
-
     let games = await Game.find(search).sort(sort(req.query.sort));
     games = gamesMapper.mapToGamesList(games);
     resp.json(games);
